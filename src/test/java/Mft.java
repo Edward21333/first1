0acac382ac298c9e2882a03a1f07cb428f2913e7
Edward <jordanbelfort@ws.us>
2020-11-15 14:29:43 +0200

    number change

--- src/test/java/Mft.java
+++ src/test/java/Mft.java
@@ -5,7 +5,7 @@ public class Mft {
     public void firstTest() {
         System.out.println("hello, Corona");
 
-        int sum = twoNumberNine(14, 88);
+        int sum = twoNumberNine(148, 88);
         System.out.println(sum);
         System.out.println(twoNumberNine(13, 37));
     }
